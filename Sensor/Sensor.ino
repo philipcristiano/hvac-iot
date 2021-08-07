146a6cf5abe7bf0821cc6ce1b5dba145b7c8f2e0
Philip Cristiano <git@philipcristiano.com>
2021-08-07 19:06:30 -0400

    Adjust frequency

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -14,7 +14,7 @@
 /************ Radio Setup ***************/
 
 // Change to 434.0 or other frequency, must match RX's freq!
-#define RF69_FREQ 915.0
+#define RF69_FREQ 905.0
 
 // Where to send packets to!
 #define DEST_ADDRESS   1
